a2a442a2b806f35755b1ff4896061cc41c0aff54
Urolit <48915495+Urolit@users.noreply.github.com>
2020-02-12 19:21:19 +0100

    finito
    dopo molta sofferenza

--- primaprova.c
+++ primaprova.c
@@ -1,141 +1,143 @@
 #include <stdio.h>
+#include <string.h>
+#include <malloc.h>
 
-struct Lettera{
-    char carattere;
-    int frequenza;
-    struct Lettera* next;
-};
-
-//metodo per aggiungere un nuovo nodo o creare la lista
-//TODO mancano tutti i malloc eheheh
-void add(struct Lettera*, char);
-void add(struct Lettera *f, char c){
-    //se la lista è vuota creo un nuovo nodo
-    if(isEmpty(f)){
-        struct Lettera nuovo;
-        nuovo.carattere = c;
-        nuovo.frequenza = 1;
-        nuovo.next = NULL;
-        *f = nuovo;
-    }else
-    {
-        struct Lettera toadd = *cerca(f,c);
-        //se esiste la lettera mi faccio passare il puntatore da cerca
-        //e incremento la frequenza.
-        if (toadd.carattere != NULL)
-        {
-            toadd.frequenza++;
-            return;
-        }else 
-        {
-            //quindi se non è presente il carattere scorro la lista e lo aggiungo
-            //scorro la lista fino in fondo
-            
-            while(toadd.next != NULL){
-                toadd = *toadd.next;
+/*frase, grandezza, array*/
+void creaArr(char*,int,int*);
+void creaArr(char* frase, int n, int *arr){
+    int cont = 0;
+    while(cont != n){
+        char c = frase[cont];
+        if(c == ' '){
+            arr[cont] = 0;
+            cont++;
+            continue;
+        }
+        int frequenza = 0;
+        //conto la frequenza del carattere c
+        for(int i = 0; i < n; i++){
+            if(frase[i] == c){
+                frequenza++;
             }
-            //creo una struttura e l'indirizzo lo metto nel puntatore next dell'ultima struct
-            struct Lettera nuovo;
-            nuovo.carattere = c;
-            nuovo.frequenza = 1;
-            nuovo.next = NULL;
-            toadd.next = &nuovo;
         }
-        
-    }
-    
-}
-//controllo che la lista di lettera non sia vuota
-int isEmpty (struct Lettera*) ;
-int isEmpty (struct Lettera* list) {
-    return list == NULL ;
-}
-
-//metodo che cerca nella lista se è presente il carattere
-struct Lettera* cerca(struct Lettera*,char );
-struct Lettera* cerca(struct Lettera *f,char c){
-    //struct Lettera tc = *f; //to check
-    if(f->carattere == c){
-        return &f;}
-    if(f->next == NULL){
-        return NULL;}
-    else{
-        return cerca(f->next,c);
-    }     
-}
-
-//ordina la lista in base alla frequenza così da scorrerla in maniera ottimale
-//quando vado a scambiare i caratteri
-void ordinaLista(struct Lettera *,struct Lettera*);
-void ordinaLista(struct Lettera *disordinata, struct Lettera *ordinata){
-    struct Lettera* pnav = disordinata;
-    int navigatore = 1;
-    int maxfreq= 0;
-    while (pnav->next != NULL && navigatore != maxfreq)
-    {
-        //per evita di uscire troppo presto
-        if (pnav->frequenza > maxfreq){
-            maxfreq = pnav->frequenza; 
-        }
-
-        if (pnav->frequenza == navigatore)
-        {
-            add(ordinata,pnav->carattere);
-            pnav = pnav->next;
-        }else
-        {
-            pnav = pnav->next;
-        }
-        
-        if (pnav->next == NULL && navigatore < maxfreq )
-        {
-            navigatore++;
-            pnav = disordinata;
-        }   
+        arr[cont] = frequenza;
+        cont++;
     }
     return;
 }
-//f = frase && gs = given string;
-//il metodo che scambia le lettere e ritorna la stringa risolta
-char crypt(struct Lettera,char*);
-char crypt(struct Lettera f, char *gs){
-    //ricordati il malloc per creare una stringa lunga come quella data.
-    /* per ogni carattere nella stringa gs chiamo f.add,
-     una volta completato ordino la lista
-     poi scorro la lista ordinata sostituendo il primo carattere che trovo
-     con l'ultimo carattere con la stessa frequenza
-     A B C D E
-     1 1 1 2 2
-     per esempio scambio la A con la C e viceversa nella stringa gs, poi le rimuovo
-     la B da sola viene ignorata, poi viene sostituita la D con la E e viceversa.
-     quando la lista non ha altri valori ritorno la stringa trasformata.
-    */
-    for(int i = 0; i < strlen(gs); i++){
-        add(&f,gs[i]);
+
+/*frase, array, grandezza, soluzione*/
+void crypt(char*,int*, int,char*);
+void crypt(char* frase, int* arr, int size, char* sol){
+    printf("\n\n--- crypt ---\n\n");
+    //se scorro la frase devo eliminare le lettere che sostituisco altrimenti 
+    //si invertono a numeri pari e tornano come erano all'inizio eheh
+    int contatore = 0;
+    char carattere;
+    int frequenza = 0;
+    int i = 0;
+    while(contatore < size/2 + 1){
+        printf("\n--crypt-- carattere num = %d",contatore);
+        if(frase[contatore] == ' '){
+            contatore++;
+            continue;
+        }
+        carattere = frase[contatore];
+        frequenza = arr[contatore];
+        printf("\n--crypt-- char = %c freq= %d",carattere,frequenza);
+
+        //partendo dal fondo scorro le posizioni i
+        for(i = size-1; 0 <= i; i--){ //to remove il -1
+            
+            //se non trovo altri con stessa frequenza prima di contatore allora continuo
+            //scherzavo.. non funziona
+            if(contatore == i){
+                contatore++;
+                i = 0;
+            }
+            
+            //se il carattere ha la stessa frequenza ed è diverso da se stesso o " " allora:
+            //li sostituisco ed tolgo i caratteri altrimenti continuo a scorrere frase;
+            if(arr[i]==frequenza && frase[i]!=carattere && frase[i]!=' '){
+                printf("\nfrase = %s",frase);
+                printf("\nsol = %s",sol);
+                trapianta(sol, size, carattere, frase[i]);
+                elimina(frase,arr,size,carattere,frase[i]);
+                printf("\n--diventano--");
+                printf("\nfrase = %s",frase);
+                printf("\nsol = %s",sol);
+                contatore++;
+                i = 0; //or break ?
+            }
+        }
     }
-    struct Lettera ordinata;
-    ordinaLista(&f, &ordinata);
-    
-    /*
-    se il primo nodo che prendo non ha next con frequenza uguale allora lo rimuovo
-    ovvero *f = *f->next;
-    prendo il primo elemento di ordinata
-    inizializzo carattere e frequenza.
-    scorro ordinata finche navig->next->frequenza != dalla frequenza iniziale
-    e lo inizializzo per andare poi a sostituirlo.
-    e al puntatore di Lettera gli assegno la memoria del primo->next,
-    al puntatore navigatore precedente faccio navprec->next = navprec->next->next;
-    cosi tolgo i 2 elementi dalla lista e continuo
-    allora nella stringa GS cambio ogni lettera del carattere iniziale
-    con il carattere del nodo finale.
-    successivamente riapro *f e ripeto quello sopra
-    */
 }
 
-main(){
-    struct Lettera frase;
-    char toChange = {"casa"};
-    char criptedString;
-    printf("frase da criptare : %s",toChange);
-    printf("la frase criptata e' : %s",criptedString);
+/* frase, ada , grandezza, char 1 e char 2 da togliere*/
+void elimina(char*,int*, int, char,char);
+void elimina(char* stringa,int* arr, int size, char tc, char sost){
+    printf("\nelimina - tc = %c\te\tsost = %c",tc,sost);
+    //scorro la stringa e ogni volta che trovo il carattere, in quella posizione 
+    //metto ' ' e frequenza 0
+    int i = 0;
+    for(i = 0; i < size; i++){
+        if(stringa[i] == tc || stringa[i] == sost){
+            stringa[i] = ' ';
+            arr[i] = 0;
+        }
+    }
+}
+
+/* soluzione, grandezza, char 1, char 2*/
+void trapianta(char*,int, char,char);
+void trapianta(char* stringa,int n, char tc, char sost){
+    printf("\ntransplant - tc = %c\tsost = %c",tc,sost);
+    //per tutta la lunghezza della stringa
+    for(int i = 0; i < n; i++){
+        //se trovo il primo carattere lo cambio col secondo
+        if(stringa[i] == tc){
+            stringa[i] = sost;
+        }else
+        {   //e se trovo il secondo lo cambio con il primo
+            if(stringa[i] == sost){
+            stringa[i] = tc;
+            }
+        }
+        
+    }
+}
+
+
+int main(void){
+    //creo le variabili
+    int size;
+    char *frase;
+    int *ada = NULL;
+    char *soluzione;
+    printf("\nsize = %d",size);
+    printf("\ninserire la frase da criptare: ");
+    scanf ("%[^\n]%*c", frase); //prova se funziona sennò quello sotto
+    //scanf("%s",frase); 
+    printf("\nla frase da criptare: %s",frase);
+    //alloco gli array di char
+    size = strlen(frase);
+    printf("\nsize = %d",size);
+
+    ada = malloc(size * sizeof(int));
+    soluzione = malloc(size * sizeof(char));
+
+    strcpy(soluzione,frase);
+    creaArr(frase,size,ada);
+
+    //vedo se è corretto 
+    for(int i = 0; i < size; i++){
+        printf("\nada[%d] = %d",i,ada[i]);
+    };
+    
+    crypt(frase,ada,size,soluzione);
+
+    printf("\necco la frase criptata: %s\n",soluzione);
+    free(soluzione);
+    free(ada);
+    return 0;
 }
